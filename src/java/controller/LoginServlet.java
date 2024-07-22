4e1aa32c9c9273bf7f6100d2e7214e4ccbd0903d
Erikrussian <caohoanglinh203@gmail.com>
2024-07-22 23:30:45 +0700

    Merge origin/master into test2
    #	build/web/home.jsp
    #	web/home.jsp

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -8,7 +8,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import model.CartItem;
 import model.Shop;
 import model.User;
@@ -40,6 +43,7 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         response.sendRedirect("./login.jsp");
     }
+    private final OrderRepository orderRepository = new OrderRepository();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -56,26 +60,52 @@ public class LoginServlet extends HttpServlet {
             request.setAttribute("error", ms);
             request.getRequestDispatcher("./login.jsp").forward(request, response);
         } else if (password.equals(c1.getPassword())) {
-            if (c1.getBanstatus() == true){
+            if (c1.getBanstatus() == true) {
                 response.sendRedirect("ban.jsp");
             } else {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", c1);
-            session.setMaxInactiveInterval(864000); // 1440 phút = 24 giờ
-            Shop shop = shopr.getShopByUserid(c1.getUserid());
-            List<CartItem> cart = cb1.getCartItemsByUserId(c1.getUserid());
-            int cartsize = cart.size();
-            session.setAttribute("cartsize", cartsize);
-            if (shop != null) {
-                session.setAttribute("shop", shop);
-            }
-            response.sendRedirect("./listProduct");
+                HttpSession session = request.getSession();
+                session.setAttribute("user", c1);
+                session.setMaxInactiveInterval(864000); // 1440 phút = 24 giờ
+                Shop shop = shopr.getShopByUserid(c1.getUserid());
+                List<CartItem> cartItems = orderRepository.getCartItemsByUserId(c1.getUserid());
+
+                List<CartItem> validCartItems = new ArrayList<>();
+                for (CartItem item : cartItems) {
+                    int availableQuantity = orderRepository.checkProductQuantity(item.getSize(), item.getColor(), item.getProductId());
+                    if (item.getQuantity() <= availableQuantity) {
+                        validCartItems.add(item);
+                    }
+                }
+
+                List<CartItem> aggregatedCartItems = aggregateCartItems(validCartItems);
+                int cartsize = aggregatedCartItems.size();
+                session.setAttribute("cartsize", cartsize);
+                if (shop != null) {
+                    session.setAttribute("shop", shop);
+                }
+                response.sendRedirect("./listProduct");
             }
         } else {
             String ms = "Sai mật khẩu";
             request.setAttribute("error", ms);
             request.getRequestDispatcher("./login.jsp").forward(request, response);
         }
+    }
+
+    private List<CartItem> aggregateCartItems(List<CartItem> cartItems) {
+        Map<String, CartItem> aggregatedMap = new HashMap<>();
+
+        for (CartItem item : cartItems) {
+            String key = item.getShopname() + "|" + item.getProductName() + "|" + item.getSize() + "|" + item.getColor();
+            if (aggregatedMap.containsKey(key)) {
+                CartItem existingItem = aggregatedMap.get(key);
+                existingItem.setQuantity(existingItem.getQuantity() + item.getQuantity());
+            } else {
+                aggregatedMap.put(key, item);
+            }
+        }
+
+        return new ArrayList<>(aggregatedMap.values());
     }
 
     @Override
